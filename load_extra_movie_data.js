d24298467a6e783b717df2c02f88b7da2966268a
Kliodt <vadimkl2004@mail.ru>
2025-09-12 00:38:43 +0300

    add data fetching on build stage

--- load_extra_movie_data.js
+++ load_extra_movie_data.js
@@ -1,4 +1,61 @@
-'use strict'
+const { argv, exit } = require('node:process');
+const fs = require('node:fs')
 
-// todo: написать скрипт 
-// todo: настроить сборку с ним
+async function fetchMovieData(imdbId) {
+    const response = await (await fetch(`https://api.imdbapi.dev/titles/${imdbId}`)).json();
+    return {
+        title: response?.primaryTitle,
+        url: `https://www.imdb.com/title/${imdbId}`,
+        poster: response?.primaryImage?.url,
+        start_year: response?.startYear,
+        end_year: response?.endYear,
+        rating: response?.rating?.aggregateRating,
+        directors: response?.directors?.map(d => ({
+           name: d?.displayName 
+        })),
+    };
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+    const srcFile = argv[2];
+    const targetFile = argv[3];
+    
+    if (!srcFile || !targetFile) {
+        console.error("Usage: node load_extra_movie_data.js <src_file> <target_file>")
+        exit(-1);
+    }
+    
+    const data = fs.readFileSync(srcFile, {encoding: 'utf-8'});
+    
+    const {config, movies} = JSON.parse(data);
+    
+    if (!config || !movies) {
+        console.error("Invalid source file format");
+        exit(-1);
+    }
+
+    for (let i = 0; i < movies.length; ++i) {
+        let imdbId = String(movies[i].imdb);
+        if (imdbId.length === 0) continue;
+        if (!imdbId.startsWith('tt')) imdbId = 'tt' + imdbId;
+        try {
+            const extraData = await fetchMovieData(imdbId);
+            movies[i] = {...extraData, ...movies[i]};
+        } catch (err) {
+            console.error(err);
+        }
+        console.log(`Done ${i+1} / ${movies.length}`);
+        await sleep(500);
+        if (i===10) break;
+    }
+    
+    config.has_extra_info = true;
+    
+    fs.writeFileSync(targetFile, JSON.stringify({config, movies}), {encoding: 'utf-8'});
+}
+
+main();
--- script.js
+++ script.js
@@ -2,52 +2,69 @@
 
 let allMovies = []
 
-
-const TierlistType = {
-    POSTERS: 1,
-    NAMES: 2,
-    NAMES_AND_POSTERS: 3
-}
-
-const PosterHeight = {
-    SMALL: '90px',
-    NORMAL: '150px',
-    LARGE: '200px'
-}
-
 function changeTierlistMode(type = null) {
+    const allowedTypes = {
+        names: 'names', 
+        posters: 'posters', 
+        comb: 'combined'
+    }
+    const getCardWithPosterHTML = (movieItem, idx) => {
+        return `<div class='poster-card' data-idx=${idx}>
+            <a href=${movieItem.url} target='_blank'>
+                <img src=${movieItem.poster} alt=${movieItem.title || 'unk'}/>
+            </a>
+        </div>`;
+    }
+    const getCardWithNameHTML = (movieItem, idx) => {
+        return `<li class='name-card' data-idx=${idx}>
+            <a href=${movieItem.url} target='_blank'>
+                ${movieItem.title}
+            </a>
+        </li>`
+    }
+    const getCardWithNameAndPoster = (movieItem, idx) => {
+        const getComment = () => {
+            return movieItem.comment || null;
+        }
+        const getYear = () => {
+            if (!movieItem.start_year) return null;
+            if (!movieItem.end_year) return `<span>${movieItem.start_year}</span>`;
+            return `<span style='white-space: nowrap;'>${movieItem.start_year} - ${movieItem.end_year}</span>`;
+        }
+        const getRate = () => {
+            const star = '<i class="fa-solid fa-star"></i>';
+            return movieItem.rating ? `<span style='white-space: nowrap;'>${star} ${movieItem.rating}</span>` : null;
+        }
+        const getTitle = () => {
+            return `<a href=${movieItem.url} target='_blank'>${movieItem.title || '???'}</a>`;
+        }
+        const getDirectors = () => {
+            return movieItem?.directors?.map(d => d.name) || [];
+        }
+        const line1 = [getTitle(), getYear(), getRate()].filter(e => e);
+        const line2 = getDirectors();
+        const line3 = [getComment()];
+        
+        return `<div class='name-w-poster-card' data-idx=${idx}>
+            <div style='flex-shrink: 0;'>
+                <a href=${movieItem.url} target='_blank'>
+                    <img src=${movieItem.poster} alt=${movieItem.title || '???'}/>
+                </a>
+            </div>
+            <div style='display: flex; flex-direction: column; gap: 3px;'>
+                <span class='details'>
+                    ${line1.join('&nbsp; &bull; &nbsp;')}
+                </span>
+                <div class='details'>
+                    ${line2.join('&nbsp; &bull; &nbsp;')}
+                </div>
+                <div class='comment'>
+                    ${line3.join('&nbsp; &bull; &nbsp;')}
+                </div>
+            </div>
+        </div>`;
+    }
     const updateTierlist = (type) => {
-        const getCardWithPosterHTML = (movieItem, idx) => {
-            return `<div class='poster-card' data-idx=${idx}>
-                <a href=${movieItem.url} target='_blank'>
-                    <img src=${movieItem.poster} alt=${movieItem.title || 'unk'}/>
-                </a>
-            </div>`;
-        }
-        const getCardWithNameHTML = (movieItem, idx) => {
-            return `<div class='name-card' data-idx=${idx}>
-                <a href=${movieItem.url} target='_blank'>
-                    ${movieItem.title}
-                </a>
-            </div>`
-        }
-        const getCardWithNameAndPoster = (movieItem, idx) => {
-            const comment = movieItem.comment ? `<span class='comment'>${movieItem.comment}</span>` : null;
-            return `<div class='name-w-poster-card' data-idx=${idx}>
-                <div style='flex-shrink: 0;'>
-                    <a href=${movieItem.url} target='_blank'>
-                        <img src=${movieItem.poster} alt=${movieItem.title || 'unk'}/>
-                    </a>
-                </div>
-                <div>
-                    <a href=${movieItem.url} target='_blank'>
-                        ${movieItem.title}
-                    </a>
-                    <br/>
-                    ${comment || ''}
-                </div>
-            </div>`;
-        }
         const tiers = {
             's': $('#tier-s'),
             'a': $('#tier-a'),
@@ -56,31 +73,44 @@ function changeTierlistMode(type = null) {
             'd': $('#tier-d'),
             '?': $('#tier-unk')
         }
-        for (const key in tiers) {
-            tiers[key].html("");
-        }
+        
+        Object.values(tiers).forEach(el => el.html(""));
+
         allMovies.forEach((el, idx) => {
             let t = el?.tier?.toLowerCase();
             if (t.length === 0) return;
             t = (t in tiers) ? t : '?';
-            
-            if (type === TierlistType.POSTERS) {
-                tiers[t].append(getCardWithPosterHTML(el, idx));
-            } else if (type === TierlistType.NAMES) {
-                tiers[t].append(getCardWithNameHTML(el, idx));
-            } else if (type === TierlistType.NAMES_AND_POSTERS) {
-                tiers[t].append(getCardWithNameAndPoster(el, idx));
+
+            switch (type) {
+                case allowedTypes.names:
+                    tiers[t].append(getCardWithNameHTML(el, idx));
+                    break;
+                case allowedTypes.posters:
+                    tiers[t].append(getCardWithPosterHTML(el, idx));
+                    break;
+                case allowedTypes.comb:
+                    tiers[t].append(getCardWithNameAndPoster(el, idx));
+                    break;
             }
         });
+
+        if (type === allowedTypes.names) {
+            Object.values(tiers).forEach(el => el.html('<ul>' + el.html() + '</ul>'));
+        }
+    }
+    const updateDocumentButtons = (type) => {
+        $('#tierlist-type-button-set button').removeClass('selected');
+        $(`#tierlist-type-button-set button[data-tag=${type}]`).addClass('selected');
     }
     
     if (type === null) {
         type = JSON.parse(localStorage.getItem("tierlist-mode"));
     }
-    if (!Object.values(TierlistType).includes(type)) {
-        type = TierlistType.POSTERS
+    if (!Object.values(allowedTypes).includes(type)) {
+        type = allowedTypes.posters;
     }
     updateTierlist(type);
+    updateDocumentButtons(type);
     localStorage.setItem("tierlist-mode", JSON.stringify(type));
 }
 
@@ -100,14 +130,19 @@ function downloadCurrentList() {
 }
 
 function changePosterHeight(posterHeight = null) {
+    const updateDocumentButtons = (tag) => {
+        $('#poster-size-button-set button').removeClass('selected');
+        $(`#poster-size-button-set button[data-tag=${tag}]`).addClass('selected');
+    }
     if (posterHeight === null) {
         posterHeight = localStorage.getItem("poster-height");
     }
-    if (!Object.values(PosterHeight).includes(posterHeight)) {
-        posterHeight = PosterHeight.NORMAL
+    if (!posterHeight.match(/^\d+px$/g)) {
+        posterHeight = '150px';
     }
     $(":root").css("--poster-height", posterHeight);
     localStorage.setItem("poster-height", posterHeight);
+    updateDocumentButtons(posterHeight);
 }
 
 async function updateImdbDataAll() {
@@ -135,17 +170,6 @@ async function updateImdbDataAll() {
         return ret;
     }
 
-    // const getImdbData_V2 = async (movieId) => {
-    //     movieId = String(movieId)
-    //     if (!movieId.startsWith('tt')) {
-    //         movieId = 'tt' + movieId;
-    //     }
-    //     // found another random semi-working public api
-    //     const response = await fetch(`https://api.imdbapi.dev/titles/${movieId}`);
-    //     // Example response:
-        
-    // }
-
     allMovies = await Promise.all(
         allMovies.map(async el => {
             let imdbData = null;
@@ -163,7 +187,9 @@ async function updateImdbDataAll() {
 async function main() {
 
     try {
-        const {config, movies} = await (await fetch("./movies.ext.json")).json();
+        let response = await fetch("./movies.ext.json");
+        if (!response.ok) response = await fetch("./movies.json");
+        const { config, movies } = await response.json();
         
         // update title
         if (config && ("title" in config)) {
@@ -177,7 +203,6 @@ async function main() {
             await updateImdbDataAll();
         }
 
-        // todo: highlight selected option
         changeTierlistMode(null);
 
         changePosterHeight(null);
@@ -185,7 +210,6 @@ async function main() {
     } catch (err) {
         console.log(err);
     }
-    
 }
 
 main();
